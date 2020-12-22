6e5a0f39b1895b2f8d7230dfba1dd4ad16ee3753
ahmadliaydin <aydin.ahmadli@merck.com>
2020-12-22 15:41:16 +0400

    added Vocab File creation

--- pre_LDA_tokenizations.py
+++ pre_LDA_tokenizations.py
@@ -6,11 +6,12 @@ from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.datasets import fetch_20newsgroups
 from nltk.tokenize import word_tokenize
 import numpy as np
+sp = spacy.load('en_core_web_sm')
+
 
 # grab a sample data set
 dataset = fetch_20newsgroups(shuffle=True, remove=('headers', 'footers', 'quotes'))
 train, test = dataset.data, dataset.data
-
 
 """
 [M] [term_1]:[count] [term_2]:[count] ...  [term_N]:[count]
@@ -19,58 +20,55 @@ train, test = dataset.data, dataset.data
 [count] - how many times each term appeared in the document.
 [term]  - integer which indexes the term; it is not a string.
 
-Assuming Movie Descriptions will be stored as string in Oracle Database
-
-
-
+DB Row Counts: [MOVIES - 27,278] [USERS - 138,493] [RATINGS - 20,000,263]
 """
 
 
-
-def nltk_tokenize(corpus):
+def nltk_token_vocab(corpus):
     """
-    corpus = ''.join(my_text)
+    corpus = ''.join(corpus)
     For now, -*- type(corpus) is str -*- (single long text, i.e combination of all movie description sentences)
     """
     start = time.time()
-    print("Corpus size:", len(corpus))
-    if type(corpus) is str:
+    corpus = ''.join(corpus)
+    if type(corpus) is str: # SURE it is str for now
         word_tokens = word_tokenize(corpus)
     else:
         word_tokens = [word_tokenize(i) for i in corpus] #2nd: list(map(word_tokenize, train2))
-    voc = set(list(word_tokens))
+
+    #TODO: Sort Vocabulary
+    #Create Vocabulary textfile
+    vocab = set(list(word_tokens))
+    with open("vocabulary.txt", 'w', encoding='utf-8') as f:
+        for word in vocab:
+            f.write(word)
+            f.write("\n")
+
+    # 20-60 seconds total execution time
+    print("Corpus size:", len(corpus))
     print("word_tokens size:", len(word_tokens))
-    print("vocab size:", len(voc))
-    print("word/voc shrinkage", int(len(word_tokens)/len(voc)))
-    print("NLTK time:", time.time() - start)
-    exit()
-    # 20-60 seconds interval
+    print("vocab size:", len(vocab))
+    print("word/voc shrinkage:", int(len(word_tokens)/len(vocab)))
+    print("NLTK time: {:.1f} seconds".format(time.time() - start))
+
+    return word_tokens, vocab
 
 
-#TODO: NLTK vs Spacy
-sp = spacy.load('en_core_web_sm')
-
+#NLTK version
 my_text = train.copy()
-for i in range(2):
-    my_text += train
+#for i in range(2):
+#    my_text += train
 
-my_text = ''.join(my_text)
-nltk_tokenize(my_text)
-
+_, vocab = nltk_token_vocab(my_text)
 
 
 
-
-
-
+#TODO: SpaCy version
 '''start = time.time()
 train2 = train.copy()
 spacy_nlp = spacy.load('en_core_web_sm')
 tokenized_sents = [[token.text for token in spacy_nlp(article)] for article in train2]
 print("spacy time:", time.time() - start)'''
-
-
-
 '''train = ["My name is name", "Your name is mine"]
 
 # vectorizer the features
--- working_with_text.py
+++ working_with_text.py
@@ -17,7 +17,7 @@ cv = CountVectorizer()
 
 x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
 
-x_traincv = cv.fit_transform(["Hi How are you How are you doing","Hi what's up","Wow that's awesome"])
+x_traincv = cv.fit_transform(["Hi How are you How are you doing", "Hi what's up","Wow that's awesome"])
 x_traincv = x_traincv.toarray()
 
 #print(type(x_traincv))
@@ -30,4 +30,4 @@ corpus = collections.Counter(a)
 
 #convert counter object to dictionary
 corpus = dict(corpus)
-print("Corpus:",corpus)
+print("Corpus:", corpus)
