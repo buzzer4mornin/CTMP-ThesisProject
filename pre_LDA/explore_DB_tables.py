200e50715e78e9ab600b951a84a6c638e6e3f921
ahmadliaydin <aydin.ahmadli@merck.com>
2021-01-08 15:20:22 +0400

    -add user_df to git -minor updates

--- pre_LDA/explore_DB_tables.py
+++ pre_LDA/explore_DB_tables.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import time
 
+# ======== Load user_df ========
+user_df = pd.read_pickle("user_df")
+
+
 # ======== Load movie_df =========
 movie_df = pd.read_pickle("movie_df")
 # Count of Movies without Plots ==> [870 / ~27k]
@@ -18,15 +22,15 @@ rating_df["RATING"] = rating_df["RATING"].apply(lambda x: 1 if x >= 4 else 0)
 # print(rating_df.head())
 
 
-# ========== TRAINING ===========
+# ========== Clean movie_df & rating_df ===========
 movie_unique = movie_df["MOVIEID"]
 rating_unique = rating_df["MOVIEID"].drop_duplicates()
 joint = pd.merge(movie_unique, rating_unique, how='inner', on='MOVIEID')
-movie_final = pd.merge(movie_df, joint, how='inner', on='MOVIEID')
-rating_final = pd.merge(rating_df, joint, how='inner', on='MOVIEID')
+movie_df = pd.merge(movie_df, joint, how='inner', on='MOVIEID')
+rating_df = pd.merge(rating_df, joint, how='inner', on='MOVIEID')
 
 print("unique MOVIEIDs after dropping NAs  [movie]   -- ", movie_unique.shape[0])
 print("unique MOVIEIDs in 20M               [rating] -- ", rating_unique.shape[0])
 print("unique MOVIEIDS jointly by  [movie]vs[rating] -- ", joint.shape[0])
 print("========================================================")
-print("DONE... TEST: ", pd.merge(rating_final, movie_final, how='inner', on="MOVIEID")["MOVIEID"].nunique())
+print("DONE... TEST: ", pd.merge(rating_df, movie_df, how='inner', on="MOVIEID")["MOVIEID"].nunique())
