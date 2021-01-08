26c85e880879c3e50b073694ea12123170931e5b
ahmadliaydin <aydin.ahmadli@merck.com>
2021-01-08 15:52:24 +0400

    pickle cleaned movie_df & rating_df

--- pre_LDA/DB_tables.py
+++ pre_LDA/DB_tables.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import time
+import os
 
 # ======== Load user_df ========
 user_df = pd.read_pickle("user_df")
@@ -15,12 +16,10 @@ print("unique MOVIEIDs before dropping NAs [movie]   -- ", movie_df.shape[0])
 movie_df = movie_df[movie_df.MOVIEPLOT != 'N/A']
 # print(movie_df.head())
 
-
 # ======== Load rating_df ========
 rating_df = pd.read_pickle("rating_df")
 rating_df["RATING"] = rating_df["RATING"].apply(lambda x: 1 if x >= 4 else 0)
 # print(rating_df.head())
-
 
 # ========== Clean movie_df & rating_df ===========
 movie_unique = movie_df["MOVIEID"]
@@ -32,5 +31,12 @@ rating_df = pd.merge(rating_df, joint, how='inner', on='MOVIEID')
 print("unique MOVIEIDs after dropping NAs  [movie]   -- ", movie_unique.shape[0])
 print("unique MOVIEIDs in 20M               [rating] -- ", rating_unique.shape[0])
 print("unique MOVIEIDS jointly by  [movie]vs[rating] -- ", joint.shape[0])
+
 print("========================================================")
 print("DONE... TEST: ", pd.merge(rating_df, movie_df, how='inner', on="MOVIEID")["MOVIEID"].nunique())
+print("========================================================")
+
+print("SAVING... cleaned movie_df & rating_df")
+movie_df.to_pickle((str(os.path.dirname(os.path.abspath(__file__))) + '/movie_df'))
+rating_df.to_pickle((str(os.path.dirname(os.path.abspath(__file__))) + '/rating_df'))
+
