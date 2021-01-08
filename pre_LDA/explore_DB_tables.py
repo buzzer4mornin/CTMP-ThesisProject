08b684013eb20845fc2118735633f088734ec3fe
ahmadliaydin <aydin.ahmadli@merck.com>
2021-01-08 12:22:46 +0400

    minor update

--- pre_LDA/explore_DB_tables.py
+++ pre_LDA/explore_DB_tables.py
@@ -10,4 +10,5 @@ plot_df = plot_df[plot_df.MOVIEPLOT != 'N/A']
 
 # ======== Load rating_df ========
 rating_df = pd.read_pickle("rating_df")
+rating_df["RATING"] = rating_df["RATING"].apply(lambda x: 1 if x >= 4 else 0)
 #print(rating_df.shape[0])
