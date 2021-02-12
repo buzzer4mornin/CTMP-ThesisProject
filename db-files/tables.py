ccca5a65a3fc007655737a39669ed0571e85f301
ahmadliaydin <aydin.ahmadli@merck.com>
2021-02-12 15:55:33 +0400

    BIG UPDATE: all files updated and added, not added files: df_rating, df_rating_CLEANED, df_rating_UPDATED

--- db-files/tables.py
+++ db-files/tables.py
@@ -9,12 +9,31 @@ os.chdir(os.path.dirname(__file__))
 # Get Current File Directory
 currdir = str(os.path.dirname(os.path.abspath(__file__)))
 
+# ======================================================================================================================
+# ======================================================================================================================
+# ============================================== DATA CLEANER ==========================================================
+'''  
+Clean raw data(coming from DataBase) 
+
+Inputs should be:
+df_user           : 138493 rows, each is unique user_id 
+df_movie          : 27278 rows, each is unique movie_id 
+df_rating         : 20000263 rows, each is unique movie_id + user_id combination 
+ 
+Outputs should be: 
+df_user_CLEANED   : 138493 rows, each is random unique user_id 
+df_movie_CLEANED  : 25900 rows, each is unique random movie_id 
+df_rating_CLEANED : 19994181 rows, each is unique random movie_id + user_id combination '''
+print("\n********************* DATA CLEANER *********************")
+
 # ======== Load df_user ========
 user_df = pd.read_pickle("df_user")
 
 
 # ======== Load df_movie =========
 movie_df = pd.read_pickle("df_movie")
+
+
 # Count of Movies without Plots ==> [870 / ~27k]
 # print(df_movie[df_movie.MOVIEPLOT == 'N/A'].shape[0])
 # Subselect Movies only with Plots
@@ -26,8 +45,7 @@ movie_df = movie_df[movie_df.MOVIEPLOT != 'N/A']
 # ======== Load df_rating ========
 rating_df = pd.read_pickle("df_rating")
 rating_df["RATING"] = rating_df["RATING"].apply(lambda x: 1 if x >= 4 else 0)
-
-# print(df_rating.head())
+# print(rating_df.head())
 
 
 # ====== Clean df_movie & df_rating ======
@@ -41,12 +59,82 @@ print("unique MOVIEIDs after dropping NAs  [movie]   -- ", movie_unique.shape[0]
 print("unique MOVIEIDs in 20M               [rating] -- ", rating_unique.shape[0])
 print("unique MOVIEIDS jointly by  [movie]vs[rating] -- ", joint.shape[0])
 
-print("========================================================")
+print("--------------------------------------------------------")
 print("DONE... TEST: ", pd.merge(rating_df, movie_df, how='inner', on="MOVIEID")["MOVIEID"].nunique())
-print("========================================================")
+print("--------------------------------------------------------")
 
-print("SAVING... cleaned df_movie & df_rating")
-movie_df.to_pickle(currdir + '/df_movie')
-rating_df.to_pickle(currdir + '/df_rating')
+print("SAVING... cleaned df_user_CLEANED & df_movie_CLEANED & df_rating_CLEANED \n")
+user_df.to_pickle(currdir + '/df_user_CLEANED')
+movie_df.to_pickle(currdir + '/df_movie_CLEANED')
+rating_df.to_pickle(currdir + '/df_rating_CLEANED')
 
-#TODO: create __main__ for prevention of double filtering of ratings table
+
+# ======================================================================================================================
+# ======================================================================================================================
+# ============================================== DATA UPDATER ==========================================================
+'''
+Update ids in cleaned data(coming from above), 
+Where user and movie ids are consistent - in range of respective table's row count.
+
+Inputs should be:
+df_user_CLEANED: random unique ids - total length of 138493
+df_movie_CLEANED: random unique ids - total length of 25900
+df_rating_CLEANED: contains random user and movie ids - total length of 19994181
+
+Outputs should be:
+df_user_UPDATED: consistent in range(138493)
+df_movie_UPDATED: consistent in range(25900)
+df_rating_UPDATED: contains updated user and movie ids - total length of 19994181
+
+DEEPNOTE: Size of input and output tables are same. Only ids are converted into consistent ones. 
+'''
+
+print("********************* DATA UPDATER *********************")
+
+# read tables
+user_df = pd.read_pickle("df_user_CLEANED")
+movie_df = pd.read_pickle("df_movie_CLEANED")
+rating_df = pd.read_pickle("df_rating_CLEANED")
+
+# add consistent user id column
+user_df["new_USERID"] = np.arange(len(user_df))
+# print(user_df)
+
+
+# add consistent movie id column
+movie_df = movie_df.drop(["TT"], axis=1)
+movie_df["new_MOVIEID"] = np.arange(len(movie_df))
+# print(movie_df)
+
+
+# convert df into np
+user_np = np.array(user_df)
+movie_np = np.array(movie_df[["MOVIEID", "new_MOVIEID"]])
+rating_np = np.array(rating_df)
+
+
+# Save Updated User and Movie tables
+print("SAVING... updated df_user_UPDATED & df_movie_UPDATED")
+user_df = user_df.drop(["USERID"], axis=1)
+movie_df = movie_df.drop(["MOVIEID"], axis=1)
+user_df.to_pickle(currdir + '/df_user_UPDATED')
+movie_df.to_pickle(currdir + '/df_movie_UPDATED')
+
+
+# Save Updated Rating table
+print(rating_np)
+s = time.time()
+user_np = user_np[:, 0].reshape(1, -1).flatten()
+movie_np = movie_np[:, 0].reshape(1, -1).flatten()
+first = lambda x: np.where(user_np == x)[0][0]
+second = lambda x: np.where(movie_np == x)[0][0]
+v_user = np.vectorize(first)
+v_movie = np.vectorize(second)
+rating_np[:, 0] = v_user(rating_np[:, 0])
+rating_np[:, 1] = v_movie(rating_np[:, 1])
+e = time.time()
+print("SAVING... updated df_rating_UPDATED || exec.time : {:.4g} min".format((e-s)/60))
+print(rating_np)
+rating_df = pd.DataFrame(rating_np)
+rating_df.columns = ["new_USERID", "new_MOVIEID", "RATING"]
+rating_df.to_pickle(currdir + '/df_rating_UPDATED')
--- pre-lda/pre_lda.py
+++ pre-lda/pre_lda.py
@@ -23,8 +23,8 @@ DB Row Counts: [MOVIES - 27,278] [USERS - 138,493] [RATINGS - 20,000,263]
 
 def get_vocabulary(plots):
     """ Create and return vocabulary out of descriptions """
-    if os.path.exists("vocab.txt"):
-        os.remove("vocab.txt")
+    if os.path.exists("ctmp_vocab.txt"):
+        os.remove("ctmp_vocab.txt")
     start = time.time()
     stop_words = set(stopwords.words('english'))
 
@@ -37,7 +37,7 @@ def get_vocabulary(plots):
 
     # Create Vocabulary textfile
     vocab = sorted(set(list(all_terms)))
-    with open("vocab.txt", 'w', encoding='utf-8') as f:
+    with open("ctmp_vocab.txt", 'w', encoding='utf-8') as f:
         for word in vocab:
             f.write(word + "\n")
     print("Total # of plots [or # of movies] :", len(plots))
@@ -45,14 +45,14 @@ def get_vocabulary(plots):
     print("Vocabulary size of all plots: {}".format(len(vocab)))
     print("Terms/Vocab shrinkage: {:.1f}".format(len(all_terms) / len(vocab)))
     print('Execution time: {:.2f} seconds'.format(time.time() - start))
-    print('-*-*-* Successfully Created "vocab.txt" *-*-*- \n')
+    print('-*-*-* Successfully Created "ctmp_vocab.txt" *-*-*- \n')
     return vocab
 
 
-def get_input(vocab, plots):
+def get_input_docs(vocab, plots):
     """ Create input text file for LDA """
-    if os.path.exists("input.txt"):
-        os.remove("input.txt")
+    if os.path.exists("ctmp_docs.txt"):
+        os.remove("ctmp_docs.txt")
     # TODO write separate def for below section??
     start = time.time()
     term_vs_index = {v_term: v_index for v_term, v_index in zip(vocab, range(len(vocab)))}
@@ -76,23 +76,23 @@ def get_input(vocab, plots):
         if unique_terms == 0:
             continue
         term_counts = str(term_counts).replace("{", "").replace("}", "").replace(" ", "").replace(",", " ")
-        with open("input.txt", 'a', encoding='utf-8') as f:
+        with open("ctmp_docs.txt", 'a', encoding='utf-8') as f:
             f.write(str(unique_terms) + " " + term_counts + "\n")
     end = time.time()
     print("Execution time: {:.2f} seconds".format(end - start))
-    print('-*-*-* Successfully Created "input.txt" *-*-*-')
+    print('-*-*-* Successfully Created "ctmp_docs.txt" *-*-*-')
 
 
 if __name__ == '__main__':
     opt = 1
     if opt == 1:
         # Incorporate df_movie [getting movie plots from DB]
-        movie_df = pd.read_pickle("../db-files/df_movie")
+        movie_df = pd.read_pickle("../db-files/df_movie_UPDATED")
         movie_plt = movie_df["MOVIEPLOT"].tolist()
 
         # Run Experiment
         vocab = get_vocabulary(movie_plt)
-        get_input(vocab, movie_plt)
+        get_input_docs(vocab, movie_plt)
 
     else:
         # Grab a sample Dataset
@@ -107,9 +107,33 @@ if __name__ == '__main__':
         _example1 = ["This is a foo_bar this sentence sentence. But also this sentence #$^&*@hello_world.123",
                      "car's solution, red's solution car , is his number of 100s 70s, 50 "]
         _example2 = [
-            "*Contains spoilers due to me having to describe some film techniques, so read at your own risk!*<br /><br />I loved this film. The use of tinting in some of the scenes makes it seem like an old photograph come to life. I also enjoyed the projection of people on a back screen. For instance, in one scene, Leopold calls his wife and she is projected behind him rather than in a typical split screen. Her face is huge in the back and Leo's is in the foreground.<br /><br />One of the best uses of this is when the young boys kill the Ravensteins on the train, a scene shot in an almost political poster style, with facial close ups. It reminded me of Battleship Potemkin, that intense constant style coupled with the spray of red to convey tons of horror without much gore. Same with the scene when Katharina finds her father dead in the bathtub...you can only see the red water on the side. It is one of the things I love about Von Trier, his understatement of horror, which ends up making it all the more creepy.<br /><br />The use of text in the film was unique, like when Leo's character is pushed by the word, 'Werewolf.' I have never seen anything like that in a film.<br /><br />The use of black comedy in this film was well done. Ernst-Hugo Järegård is great as Leo's uncle. It brings up the snickers I got from his role in the Kingdom (Riget.) This humor makes the plotline of absurd anal retentiveness of train conductors against the terrible backdrop of WW2 and all the chaos, easier to take. It reminds me of Riget in the way the hospital administrator is trying to maintain a normalcy at the end of part one when everything is going crazy. It shows that some people are truly oblivious to the awful things happening around them. Yet some people, like Leo, are tuned in, but do nothing positive about it.<br /><br />The voice over, done expertly well by Max von Sydow, is amusing too. It draws you into the story and makes you jump into Leo's head, which at times is a scary place to be.<br /><br />The movie brings up the point that one is a coward if they don't choose a side. I see the same idea used in Dancer in the Dark, where Bjork's character doesn't speak up for herself and ends up being her own destruction. Actually, at one time, Von Trier seemed anti-woman to me, by making Breaking the Waves and Dancer, but now I know his male characters don't fare well either! I found myself at the same place during the end of Dancer, when you seriously want the main character to rethink their actions, but of course, they never do!"]
+            "*Contains spoilers due to me having to describe some film techniques, so read at your own risk!*<br "
+            "/><br />I loved this film. The use of tinting in some of the scenes makes it seem like an old photograph "
+            "come to life. I also enjoyed the projection of people on a back screen. For instance, in one scene, "
+            "Leopold calls his wife and she is projected behind him rather than in a typical split screen. Her face "
+            "is huge in the back and Leo's is in the foreground.<br /><br />One of the best uses of this is when the "
+            "young boys kill the Ravensteins on the train, a scene shot in an almost political poster style, "
+            "with facial close ups. It reminded me of Battleship Potemkin, that intense constant style coupled with "
+            "the spray of red to convey tons of horror without much gore. Same with the scene when Katharina finds "
+            "her father dead in the bathtub...you can only see the red water on the side. It is one of the things I "
+            "love about Von Trier, his understatement of horror, which ends up making it all the more creepy.<br "
+            "/><br />The use of text in the film was unique, like when Leo's character is pushed by the word, "
+            "'Werewolf.' I have never seen anything like that in a film.<br /><br />The use of black comedy in this "
+            "film was well done. Ernst-Hugo Järegård is great as Leo's uncle. It brings up the snickers I got from "
+            "his role in the Kingdom (Riget.) This humor makes the plotline of absurd anal retentiveness of train "
+            "conductors against the terrible backdrop of WW2 and all the chaos, easier to take. It reminds me of "
+            "Riget in the way the hospital administrator is trying to maintain a normalcy at the end of part one when "
+            "everything is going crazy. It shows that some people are truly oblivious to the awful things happening "
+            "around them. Yet some people, like Leo, are tuned in, but do nothing positive about it.<br /><br />The "
+            "voice over, done expertly well by Max von Sydow, is amusing too. It draws you into the story and makes "
+            "you jump into Leo's head, which at times is a scary place to be.<br /><br />The movie brings up the "
+            "point that one is a coward if they don't choose a side. I see the same idea used in Dancer in the Dark, "
+            "where Bjork's character doesn't speak up for herself and ends up being her own destruction. Actually, "
+            "at one time, Von Trier seemed anti-woman to me, by making Breaking the Waves and Dancer, but now I know "
+            "his male characters don't fare well either! I found myself at the same place during the end of Dancer, "
+            "when you seriously want the main character to rethink their actions, but of course, they never do!"]
         _example3 = ["Place is near restaurant, near the corner"]
 
         # Run Experiment
         my_vocabulary = get_vocabulary(my_text)
-        get_input(my_vocabulary, my_text)
+        get_input_docs(my_vocabulary, my_text)
