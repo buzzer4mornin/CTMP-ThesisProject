926e9ae689c57eb1f961b2a709c8ee429175a8d0
ahmadliaydin <aydin.ahmadli@merck.com>
2021-03-23 20:26:39 +0400

    updated DIMINISHED IDS (ids should start from 0, not 1)

--- db-files/tables-diminished.py
+++ db-files/tables-diminished.py
@@ -20,9 +20,9 @@ currdir = str(os.path.dirname(os.path.abspath(__file__)))
 # ------- Diminished Movie -------
 movie_df = pd.read_pickle("df_movie_UPDATED")
 
-# [1, 47, 119, 358, 521]
+# [1, 47, 119, 358, 521] - movies with these indeces are selected for DIMINISHED datasets
 movie_dim = movie_df.iloc[[1, 47, 119, 358, 521], :]
-movie_dim.loc[:, "new_MOVIEID"] = range(1, 6)
+movie_dim.loc[:, "new_MOVIEID"] = range(0, 5)
 movie_dim.to_pickle(currdir + '/df_movie_DIMINISHED')
 
 # Print length of each document (TOTAL 5 docs)
@@ -31,14 +31,14 @@ movie_dim.to_pickle(currdir + '/df_movie_DIMINISHED')
 
 # ------- Diminished User -------
 user_df = pd.read_pickle("df_user_UPDATED")
-user_dim = user_df.iloc[[1, 2, 3, 4, 5], :]
+user_dim = user_df.iloc[[0, 1, 2, 3, 4], :]
 user_dim.to_pickle(currdir + '/df_user_DIMINISHED')
 
 
 # ------- Diminished Rating -------
 rating_df = pd.read_pickle("df_rating_UPDATED")
-users = [i for i in list(range(1, 6)) for _ in range(5)]
-movies = [i for _ in range(5) for i in list(range(1, 6))]
+users = [i for i in list(range(0, 5)) for _ in range(5)]
+movies = [i for _ in range(5) for i in list(range(0, 5))]
 ratings = [0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0 , 1, 1, 0, 0, 1, 0, 0, 1, 1,  0, 0, 0, 0, 0]
 rating_dim = pd.DataFrame(users, columns=["new_USERID"])
 rating_dim["new_MOVIEID"] = movies
